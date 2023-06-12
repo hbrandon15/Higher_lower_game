579a6cc0880fc1b48fc17e89eff51a0adc5cd5d0
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2023-06-12 14:51:00 -0500

    Successfully compared two items based on followers

--- main.py
+++ main.py
@@ -23,18 +23,20 @@ def get_values(selected_dict):
 def compare_followers(A_followers, B_followers, user_ans):
 	"""This function compares two follow counts. If the user guessed the correct option, the function returns 1 otherwise 0."""
 	if(A_followers > B_followers and user_ans == "A"):
+		print(f"{A_followers} is greater than {B_followers}!")
 		return 1
 	elif(B_followers > A_followers and user_ans == "B"):
+		print(f"{B_followers} is greater than {A_followers}!")
 		return 1 
 	else: 
 		return 0
 
 
 def guess(A_followers, B_followers):
-	user_guess = input("Who has more followers?").upper()
+	user_guess = input("Who has more followers?\n").upper()
 	comparison_results = compare_followers(A_followers, B_followers, user_guess)
 	if(comparison_results == 1):
-		print("You are correct")
+		print(f"You are correct!")
 	else: 
 		print("You are not correct")
 
@@ -45,21 +47,16 @@ def game_start():
 	print(art.logo)
 	item1 = get_game_items()[0]
 	item2 = get_game_items()[1]
-	print(f"item 1 is {item1}")
-	print(f"item 2 is {item2}")
-	
 
-	# while(item1 == item2):
-	# 	item2 = get_game_item()
-	# else:
-	# 	item1_values = get_values(item1)
-	# 	item2_values  = get_values(item1)
-	# 	follow_count_A = item1_values[1]
-	# 	follow_count_B = item2_values[1]
-	# 	print(f"Compare A: {item1_values[0]}, a {item1_values[2]} from {item1_values[3]}\n")
-	# 	print(art.vs)
-	# 	print(f"Compare B: {item2_values[0]}, a {item2_values[2]} from {item2_values[3]}\n")
-	# 	guess(follow_count_A, follow_count_B)
+	item1_values = get_values(item1)
+	item2_values  = get_values(item2)
+
+	follow_count_A = item1_values[1]
+	follow_count_B = item2_values[1]
+	print(f"Compare A: {item1_values[0]}, a {item1_values[2]} from {item1_values[3]}\n")
+	print(art.vs)
+	print(f"Compare B: {item2_values[0]}, a {item2_values[2]} from {item2_values[3]}\n")
+	guess(follow_count_A, follow_count_B)
 		
 
 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+	print(f"Compare A: {item1_values[0]}, a {item1_values[2]} from {item1_values[3]}\n")
+	print(art.vs)
+	print(f"Compare B: {item2_values[0]}, a {item2_values[2]} from {item2_values[3]}\n")
